96ff87ff1537e827e0d319e96cad7750126ef4b3
Mariam Mohammed <76567004+Mariam1341@users.noreply.github.com>
2022-10-16 21:29:56 +0200

    update

--- main.h
+++ main.h
@@ -21,8 +21,8 @@ typedef struct format
 	char *id;
 	int (*f)();
 } convert_match;
-
-
+int printf_dec(va_list args);
+int printf_int(va_list args);
 int _strlen(char *s);
 int _strlenc(const char *s);
 int _strlenc(const char *s);
