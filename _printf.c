3cc46c93e93ba9c0331f38ca81729e7d5a05f372
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-10-16 21:09:18 +0200

    done

--- _printf.c
+++ _printf.c
@@ -8,8 +8,7 @@ int _printf(const char * const format, ...)
 {
 	convert_match m[] = {
 		{"%s", ptintf_string}, {"%c", printf_char},
-		{"%%", printf_precentage},
-		{"%i", printf_int}, {"%d", printf_dec}
+		{"%%", printf_precentage}, {"%i", printf_int}, {"%d", printf_dec}
 	};
 
 	va_list args;
@@ -22,7 +21,7 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 2;
+		j = 4;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
