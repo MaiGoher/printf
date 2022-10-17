ba9eda2df0ce4397ee62f505a32c46477ba7fe21
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-10-17 23:11:07 +0200

    done

--- main.h
+++ main.h
@@ -26,7 +26,6 @@ int printf_dec(va_list args);
 int printf_int(va_list args);
 int _strlen(char *s);
 int _strlenc(const char *s);
-int _strlenc(const char *s);
 int printf_precentage(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
